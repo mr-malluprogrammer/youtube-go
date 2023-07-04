8d75c5b18f1534984ef8f8ff66209cc637b7b621
mrmalluprogrammer <136502954+mrmalluprogrammer@users.noreply.github.com>
2023-07-04 20:47:10 +0530

    Added

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Book struct {
+	Title string	`json:"title"`
+	Author string	`json:"author"`
+	Pages int	`json:"pages"`
+
+}
+
+func Hello(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","text/html")
+	w.Write([]byte("<h1 style='color: steelblue'>Hello Mr.MalluProgrammer</h1>"))
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	book := Book {
+		Title: "The Gunslinger",
+		Author:  "Mr.MalluProgrammer",
+		Pages: 12,
+	}
+	json.NewEncoder(w).Encode(book)
+}
 
 func main() {
-	fmt.Println("Hello, Mr.MalluProgrammer")
+	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/book", GetBook)
+	log.Fatal(http.ListenAndServe(":5100",nil))
 }
