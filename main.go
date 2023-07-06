55bc80e5eab3507223fbfed23ee35afb0a7222a7
mrmalluprogrammer <136502954+mrmalluprogrammer@users.noreply.github.com>
2023-07-06 20:27:55 +0530

    added

--- main.go
+++ main.go
@@ -27,63 +27,62 @@ type Author struct {
 
 var books []Book
 
-// Easy Part
-
+// Getting all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "Successfully got the results \n")
 	json.NewEncoder(w).Encode(books)
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	// params := r.URL.Query().Get("id")
-	params := mux.Vars(r)
-	// fmt.Println(params)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			fmt.Println(books[index+1:])
-			books = append(books[:index], books[index+1:]...)
-			fmt.Fprintf(w, "Successfully Removed %s\n", item.Name)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
+// Getting a single book
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range books {
 		if item.ID == params["id"] {
+			fmt.Fprintf(w, "Successfully got result \n")
 			json.NewEncoder(w).Encode(item)
 			break
 		}
 	}
 }
 
+// Creating a book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
 	book.ID = strconv.Itoa(rand.Intn(1000000))
 	books = append(books, book)
+	fmt.Fprintf(w, "Successfully added \n")
 	json.NewEncoder(w).Encode(book)
 }
 
+// Update a book
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	// params := r.URL.Query().Get("id")
 	params := mux.Vars(r)
-	// fmt.Println(params)
 	for index, item := range books {
 		if item.ID == params["id"] {
 			books = append(books[:index], books[index+1:]...)
 			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
+			_= json.NewDecoder(r.Body).Decode(&book)
 			book.ID = params["id"]
 			books = append(books, book)
+			fmt.Fprintf(w, "Successfully updated \n")
 			json.NewEncoder(w).Encode(book)
-			break
+		}
+	}
+}
+
+// Delete a book
+func deleteBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			fmt.Fprintf(w, "Deleted the data successfully")
 		}
 	}
 	json.NewEncoder(w).Encode(books)
@@ -101,12 +100,13 @@ func main() {
 	json.Unmarshal(byteValue, &books)
 
 	r.HandleFunc("/books", getBooks).Methods("GET")
-	r.HandleFunc("/book", createBook).Methods("POST")
 	r.HandleFunc("/book/{id}", getBook).Methods("GET")
-	r.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
+	r.HandleFunc("/book", createBook).Methods("POST")
 	r.HandleFunc("/book/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
 
-	fmt.Println("Server started: http://localhost:5020/")
+	// Starting a server
+	fmt.Println("Server started: http://localhost:5020")
 	log.Fatal(http.ListenAndServe(":5020", r))
 
 }
