fa595f72548cb8e3cc6d2784377d8385968a7c55
mrmalluprogrammer <136502954+mrmalluprogrammer@users.noreply.github.com>
2023-07-06 17:10:04 +0530

    Added

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-
 	"github.com/gorilla/mux"
 )
 
@@ -28,8 +27,33 @@ var books []Book
 func getBooks(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
-	
 }
+
+func deleteBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books{
+		if item.ID == params["id"]{
+			books=append(books[:index], books[index+1:]...)
+			fmt.Fprintf(w, "Successfully Removed %s\n", item.Name)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books{
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			break
+		}
+	}
+}
+
+
 
 func main() {
 	r := mux.NewRouter()
@@ -44,7 +68,10 @@ func main() {
 
 
 	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/book/{id}", deleteBook).Methods("DELETE")
 
+	fmt.Println("Server started: http://localhost:5020/")
 	log.Fatal(http.ListenAndServe(":5020",r))
 
 }
